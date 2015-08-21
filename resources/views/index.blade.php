fb2dfdbf09388683fc9c2df1e6abeb10d61bfdd8
Yung <yung.tk17@gmail.com>
2015-08-22 00:31:56 +0800

    added simple home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,19 @@
-<html>
-	<head>
-	</head>
-	<body background="{{ URL::asset('images/515.jpg') }}">
+@extends('app')
 
-	</body>
-</html>
+@section('include')
+<link href="{{ URL::asset('css/IndexStyle.css') }}" rel="stylesheet" type="text/css" >
+@endsection
+
+@section('content')
+<div class="container-fluid header">
+	<div class="container">
+    	<p>Welcome aboard</p>
+    	<p>Your dream is coming true at this moment</p>
+	</div>
+</div>
+<div class="container-fluid footer">
+	<div class="container">
+    	
+	</div>
+</div>
+@endsection

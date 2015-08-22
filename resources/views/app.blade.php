24f28e82e3828410fbb03b6481a4b7d984b6bb59
Yung <yung.tk17@gmail.com>
2015-08-22 22:53:01 +0800

    login as

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,14 @@
 		<title>Cruises Booking System</title>
 		<meta name="_token" content="{{ csrf_token() }}"/>
 		<script type="text/javascript" src="{{ URL::asset('js/jquery-2.1.4.min.js') }}"></script>
+		<script type="text/javascript" src="{{ URL::asset('css/bootstrap-3.3.5-dist/js/bootstrap.min.js') }}"></script>
+		<link href="{{ URL::asset('css/font-awesome-4.3.0/css/font-awesome.min.css') }}" rel="stylesheet">
 		<link href="{{ URL::asset('css/bootstrap-3.3.5-dist/css/bootstrap.min.css') }}" rel="stylesheet" />	
 		<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 		@yield('include')
 	</head>
 	<body>
+		@yield('script')
 		@yield('content')
 	</body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,51 @@
 <link href="{{ URL::asset('css/IndexStyle.css') }}" rel="stylesheet" type="text/css" >
 @endsection
 
+@section('script')
+<script>
+$(document).ready(function() {
+ });
+</script>
+@endsection
+
 @section('content')
 <div class="container-fluid header">
 	<div class="container">
     	<h1>Welcome aboard</h1>
     	<h2>Your dream is coming true at this moment</h2>
-    	<a href='/login' class='btn btn-outline-inverse btn-lg'>
+    	<a class='btn btn-outline-inverse btn-lg' data-toggle="modal" data-target="#loginModal">
             <font> Login To Book Your Ticket Now</font>
         </a>
 	</div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+    	<div class="modal-content">
+      		<div class="modal-header">
+        		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        		<h4 class="modal-title" id="myModalLabel">Login as</h4>
+      		</div>
+      		<div class="modal-body">
+      			<div class="container-fluid">
+      				<div class="row">
+      					<div class="col-sm-4" id="google">
+      						<i class="fa fa-google-plus-square fa-4x" id="google-icon" ></i> <label>Google Plus</label>
+      					</div>
+      					<div class="col-sm-4" id="facebook">
+      						<i class="fa fa-facebook-square fa-4x" id="facebook-icon" ></i> <label>Facebook</label>
+      					</div>
+      					<div class="col-sm-4" id="twitter">
+      						<i class="fa fa-twitter-square fa-4x" id="twitter-icon" ></i> <label>Twitter</label>
+      					</div>
+      				</div>
+      			</div>
+    		</div>
+    		<div class="modal-footer">
+        		<button type="button" class="btn btn-default" data-dismiss="modal">Back</button>
+      		</div>
+    	</div>
+  	</div>
 </div>
 @endsection

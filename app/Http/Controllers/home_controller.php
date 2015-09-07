071e77edcb6f39e4dcfc742fc1a692cd2adbc241
Yung <yung.tk17@gmail.com>
2015-09-07 23:30:52 +0800

    fix twitter login bug

--- app/Http/Controllers/home_controller.php
+++ app/Http/Controllers/home_controller.php
@@ -113,12 +113,14 @@ class home_controller extends Controller
 			}
 			else{
 				
-				$request_token = [];
-				$request_token['oauth_token'] = $_SESSION['oauth_token'];
-				$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
-				$connection = new TwitterOAuth(Config::get('twitter.appid'), Config::get('twitter.secret'), $request_token['oauth_token'], $request_token['oauth_token_secret']);
-				$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
-				$_SESSION['access_token'] = $access_token;
+				if(isset($_REQUEST['oauth_verifier'])){
+					$request_token = [];
+					$request_token['oauth_token'] = $_SESSION['oauth_token'];
+					$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+					$connection = new TwitterOAuth(Config::get('twitter.appid'), Config::get('twitter.secret'), $request_token['oauth_token'], $request_token['oauth_token_secret']);
+					$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+					$_SESSION['access_token'] = $access_token;
+				}
 				
 				
 				$access_token = $_SESSION['access_token'];
--- config/twitter.php
+++ config/twitter.php
@@ -3,5 +3,5 @@
 return [
  	'appid'   	=> '8KIiSeykyrnwnxy7ytvdGRwER',
 	'secret'  	=> 'zjhuzpfZhDrVrZITjeCPPGUDXgVqSiQnTAtGDXCLnQLMTD2z4u',
-	'callback'  => 'http://127.0.0.1:8000/home',
+	'callback'  => 'http://newcruises.azurewebsites.net/home',
 ];
